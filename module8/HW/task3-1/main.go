c8549650e64185137b52b7a99ae8e6cd7ea543dd
l1c3t <steve.bogachev@gmail.com>
2020-11-09 19:17:45 +0300

    aded 3-1 and fix 3

--- module8/HW/task3-1/main.go
+++ module8/HW/task3-1/main.go
@@ -15,12 +15,13 @@
 
 package main
 
-//Первое решение, как мне кажется наиболее близкое к правильному (но не к условиям задачи)
-//все пришлось читать)))
+//Второе решение, как мне кажется наиболее близкое к заданию
 
 //из задания непонятно что сделать-то нужно - только из подсказки
 //функции не проходили
 //массивы/срезы не проходили
+//map'ы мы еще не проходили
+//все пришлось читать)))
 
 import (
 	"fmt"
@@ -31,7 +32,7 @@ func main() {
 	var money int
 	var change bool
 	var bills []int
-outLoop:
+outloop:
 	for {
 		fmt.Print("Введите номинал купюры\t")
 		fmt.Scan(&money)
@@ -39,68 +40,62 @@ outLoop:
 		switch money {
 		case 5, 10, 20:
 			bills = append(bills, money)
-			bills, change = lemonadeChange(bills)
-			fmt.Println("В кассе:")
-			fmt.Println(bills)
-			fmt.Println("Получится сдать сдачу:")
-			fmt.Println(change)
 		case 0:
-			break outLoop
+			break outloop
 		default:
 			fmt.Println("Неверно введенное значение, попробуйте еще раз")
 		}
-
 	}
+
+	bills, change = lemonadeChange(bills)
+	fmt.Println("В кассе:")
+	fmt.Println(bills)
+	fmt.Println("Получится сдать сдачу:")
+	fmt.Println(change)
+
 }
 
 func lemonadeChange(bills []int) ([]int, bool) {
 	lastCash := bills[len(bills)-1] - 5
-	sum := 0
 	change := false
 
-	switch lastCash {
-	case 0:
-		change = true
-	case 5:
-		for i, cash := range bills[:len(bills)-1] {
-			if cash == 5 {
-				bills[i] = 0
-				change = true
-				break
-			}
-		}
-	case 10:
-		for i, cash := range bills[:len(bills)-1] {
-			if cash == 10 {
-				bills[i] = 0
-				change = true
-			} else if cash == 5 {
-				bills[i] = 0
-				sum++
-			}
-			if sum == 2 {
-				change = true
-				break
-			}
-		}
-
-	case 15:
-		for i, cash := range bills[:len(bills)-1] {
-			if cash == 10 {
-				bills[i] = 0
-				sum = +2
-			} else if cash == 5 {
-				bills[i] = 0
-				sum++
-			}
-			if sum == 3 {
-				change = true
-				break
-			}
-		}
-
+	nominals := map[int]int{
+		5:  0,
+		10: 0,
 	}
 
+	for i := 1; i < (len(bills) - 1); i++ {
+
+		for _, cash := range bills[:i-1] {
+			if cash == 5 {
+				nominals[5]++
+			} else if cash == 10 {
+				nominals[10]++
+			}
+		}
+
+		switch lastCash {
+		case 0:
+			change = true
+		case 5:
+			if nominals[5] > 0 {
+				change = true
+			}
+		case 10:
+			if nominals[5] > 1 {
+				change = true
+			} else if nominals[10] > 0 {
+				change = true
+			}
+
+		case 15:
+			if nominals[5] > 2 {
+				change = true
+			} else if (nominals[10] > 1) && (nominals[5] > 0) {
+				change = true
+			}
+		}
+	}
 	var newBills []int
 	for _, cash := range bills {
 		if cash != 0 {
